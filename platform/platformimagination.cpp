5ba93ea20974e986c21de4bdcbc3a81f6c4ba619
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-03-20 12:47:48 +0200

    drm_hwcomposer: Add optional gem caching mechanism for Imagination platform
    On some platform[1] frequent calls of the drmPrimeFDToHandle/drmModeAddFB2
    may cause tremendous performance degradation. In case, if DRM driver[2] has
    only a primary plane(i.e. all composition done by OpenGL) there is no
    practical sense to import/release the same render targets per each frame.
    This patch utilizes specific Imagination field ui64Stamp of
    the IMG_native_handle_t to clearly identify buffer on which native
    handle points. Display hotplug event handling is needed to deallocate
    all handles/gem connected with a display.
    
    1. Considering hypervisor-based guest domain, using drm [2] driver.
    2. https://elixir.bootlin.com/linux/v5.0/source/drivers/gpu/drm/xen
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- platform/platformimagination.cpp
+++ platform/platformimagination.cpp
@@ -59,6 +59,77 @@ int ImaginationImporter::ConvertBoInfo(buffer_handle_t handle,
   return 0;
 }
 
+#ifdef ENABLE_GEM_HANDLE_CACHING
+ImaginationImporter::~ImaginationImporter() {
+  std::lock_guard<std::mutex> lock(cachedBoMutex_);
+  for (auto &pair : cachedBo_) {
+    DrmGenericImporter::ReleaseBuffer(&pair.second.bo);
+  }
+  cachedBo_.clear();
+}
+
+int ImaginationImporter::ImportBuffer(buffer_handle_t ghandle,
+                                      hwc_drm_bo_t *bo) {
+  IMG_native_handle_t *img_hnd = (IMG_native_handle_t *)ghandle;
+  int ret = 0;
+
+  if (!img_hnd)
+    return -EINVAL;
+
+  {
+    std::lock_guard<std::mutex> lock(cachedBoMutex_);
+    auto it = cachedBo_.find(img_hnd->ui64Stamp);
+    if (it != cachedBo_.end()) {
+      it->second.used = true;
+      *bo = it->second.bo;
+      return 0;
+    }
+  }
+  ret = DrmGenericImporter::ImportBuffer(ghandle, bo);
+  if (ret) {
+    ALOGE("could not create drm fb %d", ret);
+    return ret;
+  }
+  {
+    std::lock_guard<std::mutex> lock(cachedBoMutex_);
+
+    bo->priv = reinterpret_cast<void *>(img_hnd->ui64Stamp);
+    bo_t buffer;
+    buffer.bo = *bo;
+    buffer.needFlush = false;
+    buffer.used = true;
+
+    cachedBo_.insert(std::make_pair(img_hnd->ui64Stamp, buffer));
+  }
+  return ret;
+}
+
+int ImaginationImporter::ReleaseBuffer(hwc_drm_bo_t *bo) {
+  std::lock_guard<std::mutex> lock(cachedBoMutex_);
+  unsigned long long key = reinterpret_cast<unsigned long long>(bo->priv);
+  auto &buffer = cachedBo_[key];
+  buffer.used = false;
+  if (buffer.needFlush) {
+    DrmGenericImporter::ReleaseBuffer(&buffer.bo);
+    buffer.needFlush = false;
+    cachedBo_.erase(key);
+  }
+  return 0;
+}
+void ImaginationImporter::HandleHotplug() {
+  std::lock_guard<std::mutex> lock(cachedBoMutex_);
+  for (auto it = cachedBo_.begin(); it != cachedBo_.end();) {
+    it->second.needFlush = true;
+    if (!it->second.used) {
+      DrmGenericImporter::ReleaseBuffer(&it->second.bo);
+      it = cachedBo_.erase(it);
+    } else {
+      ++it;
+    }
+  }
+}
+#endif
+
 std::unique_ptr<Planner> Planner::CreateInstance(DrmDevice *) {
   std::unique_ptr<Planner> planner(new Planner);
   planner->AddStage<PlanStageGreedy>();
--- platform/platformimagination.h
+++ platform/platformimagination.h
@@ -16,6 +16,25 @@ class ImaginationImporter : public DrmGenericImporter {
   using DrmGenericImporter::DrmGenericImporter;
 
   int ConvertBoInfo(buffer_handle_t handle, hwc_drm_bo_t *bo) override;
+
+#ifdef ENABLE_GEM_HANDLE_CACHING
+  typedef struct bo_cached_descriptor {
+    hwc_drm_bo_t bo;
+    // Handle used by some external component(SF)
+    bool used;
+    // Deallocation needed
+    bool needFlush;
+  } bo_t;
+
+  ~ImaginationImporter() override;
+  int ImportBuffer(buffer_handle_t handle, hwc_drm_bo_t *bo) override;
+  int ReleaseBuffer(hwc_drm_bo_t *bo) override;
+  void HandleHotplug() override;
+
+ private:
+  std::map<unsigned long long, bo_t> cachedBo_;
+  std::mutex cachedBoMutex_;
+#endif
 };
 }  // namespace android
 
