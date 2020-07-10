4659caba0e1345258fdacfb893ed5f491d890f73
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-07-10 16:07:03 +0300

    drm_hwcomposer: Fix EDID fetch from DRM
    EDID fetch procedure consist of two steps:
    1) get appropriate blob_id
    2) fetch EDID blob using blob_id
    Those steps should be done sequentially, in
    other case drivers can update EDID and blob_id
    won't be actual.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- include/drmconnector.h
+++ include/drmconnector.h
@@ -39,6 +39,8 @@ class DrmConnector {
   DrmConnector &operator=(const DrmProperty &) = delete;
 
   int Init();
+  int UpdateEdidProperty();
+  int GetEdidBlob(drmModePropertyBlobPtr &blob);
 
   uint32_t id() const;
 
